b6485ada4d067d9fcd972da00462db6a41078254
artyom.pavlenko <itemka2503@gmail.com>
2022-10-23 19:43:34 +0200

    D&D: Rendering Project Lists

--- type_script_2022_edition/src/9.drag_and_drop/index.ts
+++ type_script_2022_edition/src/9.drag_and_drop/index.ts
@@ -140,4 +140,58 @@ class ProjectInput {
   }
 }
 
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.render();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.setAttribute('id', listId);
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private render() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element)
+  }
+}
+
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
