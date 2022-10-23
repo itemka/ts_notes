88bc179f40d9c1528991b7201ed39a48a92ea625
artyom.pavlenko <itemka2503@gmail.com>
2022-10-23 23:40:42 +0200

    D&D: Using a Getter

--- type_script_2022_edition/src/9.drag_and_drop/index.ts
+++ type_script_2022_edition/src/9.drag_and_drop/index.ts
@@ -233,6 +233,16 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  get persons() {
+    let people = this.project.people + ' person';
+
+    if (this.project.people !== 1) {
+      people = people + 's';
+    }
+
+    return people;
+  }
+
   constructor(private hostId: string, project: Project) {
     super('single-project', hostId, true, project.id);
 
@@ -246,7 +256,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   renderContent(): void {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
