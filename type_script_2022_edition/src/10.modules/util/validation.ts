0d06b76dd12306f867bf50fe9dc675b5b4df7e85
artyom.pavlenko <itemka2503@gmail.com>
2022-10-25 02:27:59 +0200

    Modules

---
+++ type_script_2022_edition/src/10.modules/util/validation.ts
@@ -0,0 +1,42 @@
+export interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+export function validate({
+  value,
+  required,
+  minLength,
+  maxLength,
+  min,
+  max,
+}: Validatable): boolean {
+  let isValid = true;
+  const valueLength = value.toString().trim().length;
+
+  if (required) {
+    isValid = isValid && valueLength !== 0;
+  }
+
+  if (minLength != null && typeof value === 'string') {
+    isValid = isValid && valueLength >= minLength;
+  }
+
+  if (maxLength != null && typeof value === 'string') {
+    isValid = isValid && valueLength <= maxLength;
+  }
+
+  if (min != null && typeof value === "number") {
+    isValid = isValid && value >= min;
+  }
+
+  if (max != null && typeof value === "number") {
+    isValid = isValid && value <= max;
+  }
+
+  return isValid
+}
