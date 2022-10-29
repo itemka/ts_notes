356e74205d1395d0ffaa47418670b5ed44efdc34
artyom.pavlenko <itemka2503@gmail.com>
2022-10-29 18:52:41 +0200

    Select & Share a Place: Using Axios to Fetch Coordinates for an Entered Address and add .env

--- type_script_2022_edition/src/13.select_and_share_place/index.ts
+++ type_script_2022_edition/src/13.select_and_share_place/index.ts
@@ -1,12 +1,47 @@
+import axios from "axios";
+
+type GoogleGeocodingResponse = {
+  results: {
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      }
+    }
+  }[],
+  status: 'OK' | 'ZERO_RESULTS'
+}
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-function searchAddress(event: Event) {
+async function searchAddress(event: Event) {
   event.preventDefault();
 
-  const { value: enteredAddress } = addressInput
+  const mainURL = process.env.GOOGLE_MAP_API;
+  const key = process.env.GOOGLE_MAP_PLATFORM_API_KEY;
+  const address = encodeURI(addressInput.value);
+  const url = `${mainURL}?address=${address}&key=${key}`;
 
-  console.log({ enteredAddress })
+  try {
+    const { data } = await axios.get<GoogleGeocodingResponse>(url);
+
+    if (data.status !== 'OK') {
+      throw new Error('Could not fetch location!');
+    }
+
+    const { lat, lng } = data.results[0].geometry.location
+
+    console.log({
+      data,
+      lat,
+      lng,
+      api_key: process.env.GOOGLE_MAP_PLATFORM_API_KEY,
+      map_api: process.env.GOOGLE_MAP_API,
+    })
+  } catch (error) {
+    console.error('Error in the searchAddress function', error);
+  }
 }
 
 form.addEventListener('submit', searchAddress);
