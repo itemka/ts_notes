7b79f809e31babbd58458c531aac02cc2ef09999
artyom.pavlenko <itemka2503@gmail.com>
2022-10-29 17:30:00 +0200

    Select & Share a Place: Getting User Input

--- type_script_2022_edition/src/13.select_and_share_place/index.ts
+++ type_script_2022_edition/src/13.select_and_share_place/index.ts
@@ -1,1 +1,12 @@
-console.log('')
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddress(event: Event) {
+  event.preventDefault();
+
+  const { value: enteredAddress } = addressInput
+
+  console.log({ enteredAddress })
+}
+
+form.addEventListener('submit', searchAddress);
